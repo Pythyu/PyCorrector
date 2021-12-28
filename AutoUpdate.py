01150cdd1f4f88a8bd0ab94689565489434697e9
antoine.zellmeyer <antoine.zellmeyer@epita.fr>
2021-12-28 16:24:39 +0100

    AutoUpdate : more detailed instruction

--- AutoUpdate.py
+++ AutoUpdate.py
@@ -3,6 +3,18 @@ import time
 import os
 import sys
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 def do_execv():
     """Re-execute the current process.
 
@@ -14,7 +26,7 @@ def do_execv():
     args.insert(0, sys.executable)
     if sys.platform == 'win32':
         args = ['"%s"' % arg for arg in args]
-        
+
     os.chdir(_startup_cwd)
     os.execv(sys.executable, args)
 
@@ -22,7 +34,14 @@ def CheckUpdate():
     try:
         print("AutoUpdate : check if a new version is available...\n")
         process = subprocess.Popen(["git", "pull"], stdout=subprocess.PIPE)
-        print(str(process.communicate()[0]))
+        out = str(process.communicate()[0])
+        print(out)
+        if 'Already up to date' not in out:
+            if "requirements.txt" in out:
+                print("\n"+ bcolors.WARNING + "\/!\\ requirements.txt may have been modified \/!\\")
+                print("We recommend using again " + bcolors.UNDERLINE + "python3 -m pip install -r requirements.txt" +bcolors.ENDC)
+            print(bcolors.OKGREEN + "The update is complete \\o\/ : please restart the app to apply the update" + bcolors.ENDC)
+
         print("\n#####################################\n")
         return True
     except Exception as e:
